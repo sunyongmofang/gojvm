4ea3aae0ce4dd1a716dbcfbf524b2564502745f7
sunyong <sunyongmofang@hotmail.com>
2017-07-07 11:40:12 +0800

    add class path

--- src/vm/classpath/entry_dir.go
+++ src/vm/classpath/entry_dir.go
@@ -1,28 +1,28 @@
 package classpath
 
 import (
-    "io/ioutil"
-    "path/filepath"
+	"io/ioutil"
+	"path/filepath"
 )
 
 type DirEntry struct {
-    absDir string
+	absDir string
 }
 
 func newDirEntry(path string) *DirEntry {
-    absDir, err := filepath.Abs(path)
-    if err != nil {
-        panic(err)
-    }
-    return &DirEntry{absDir}
+	absDir, err := filepath.Abs(path)
+	if err != nil {
+		panic(err)
+	}
+	return &DirEntry{absDir}
 }
 
 func (self *DirEntry) readClass(className string) ([]byte, Entry, error) {
-    fileName := filepath.Join(self.absDir, className)
-    data, err := ioutil.ReadFile(fileName)
-    return data, self, err
+	fileName := filepath.Join(self.absDir, className)
+	data, err := ioutil.ReadFile(fileName)
+	return data, self, err
 }
 
 func (self *DirEntry) String() string {
-    return self.absDir
+	return self.absDir
 }
